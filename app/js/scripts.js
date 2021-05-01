4c1efcf78db5b377a330cc846c9fc22b2588e3a1
Koliasick <abrikoskokos42@gmail.com>
2021-05-01 22:25:20 +0300

    added a lot of plotting

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,7 +1,7 @@
 const total_count = document.getElementById("total_count")
 const DATA_COUNT = 7;
 
-const labels = [
+const labels_one = [
     '12-6-14',
     '12-7-14',
     '12-8-14',
@@ -9,8 +9,8 @@ const labels = [
     '12-10-14',
     '12-11-14',
 ];
-const data = {
-    labels: labels,
+const data_one = {
+    labels: labels_one,
     datasets: [{
         label: 'visitors',
         backgroundColor: 'rgb(254, 199, 63)',
@@ -31,9 +31,9 @@ const data = {
         pointBackgroundColor: 'rgb(255, 0, 216)',
     }]
 };
-const config = {
+const config_one = {
     type: 'line',
-    data: data,
+    data: data_one,
     options: {
         interaction: {
             mode: 'index',
@@ -77,13 +77,125 @@ const config = {
         }
     }
 };
+//
+var options_two = {
+    series: [{
+        name: "Sales",
+        data: [200,320,200,400]
+    }],
+    chart: {
+        type: 'area',
+        height: 350,
+        zoom: {
+            enabled: false
+        },
+        sparkline:{
+            enabled:true
+        }
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        curve: 'straight',
+        colors: ['#ffffff']
+    },
+    labels: ['','','',''],
+    xaxis: {
+        type: 'none',
+    },
+    yaxis: {
+        opposite: true,
+        min: 0
+    },
+    legend: {
+        show: false,
+        colors: ['#ff00d8']
+    },
+    grid: {
+        show: false,
+        padding:{
+            left:0,
+            right:0,
+            bottom:0
+        }
+    },
+    fill: {
+        colors: '#f1c40f',
+        type: 'solid'
+    },
+    markers: {
+        size: 4,
+        shape: 'circle',
+        colors: ['#ff00d8']
+    }
+};
+//
+var options = {
+    series: [{
+        name: 'New',
+        data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+    }, {
+        name: 'Returning',
+        data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    }],
+    chart: {
+        type: 'bar',
+        height: 350
+    },
+    plotOptions: {
+        bar: {
+            horizontal: false,
+            columnWidth: '65%',
+            endingShape: 'rounded'
+        },
+    },
+    dataLabels: {
+        enabled: false
+    },
+    stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+    },
+    xaxis: {
+        categories: ['12-3-14', '12-4-14', '12-5-14', '12-6-14', '12-7-14', '12-8-14', '12-9-14', '12-10-14', '12-11-14'],
+    },
+    yaxis: {
+        title: {
+            text: 'Users'
+        }
+    },
+    fill: {
+        opacity: 1,
+        colors: ['#34495e','#1efe00']
+    },
+    tooltip: {
+        y: {
+            formatter: function (val) {
+                return val + " users"
+            }
+        }
+    },
+    legend: {
+        colors: ['#34495e','#1efe00'],
+        markers: {
+            fillColors: ['#34495e','#1efe00'],
+        }
+    }
+};
+//
 window.addEventListener('load', function () {
     update_counters();
     var bigChart = new Chart(
         document.getElementById('big_graph'),
-        config
+        config_one
     );
     document.getElementById('reload').addEventListener('click',reload);
+    var chart = new ApexCharts(document.querySelector("#area_graph"), options_two);
+    var bar_chart = new ApexCharts(document.querySelector("#bar_chart"), options);
+    chart.render();
+    bar_chart.render();
     bigChart.options.animation = false;
 })
 function update_counters()
@@ -96,4 +208,32 @@ function update_counters()
 }
 function reload(){
     location.reload();
+}
+
+google.charts.load('current', {
+    'packages':['geochart'],
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+});
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+        ['Country','Percent of users'],
+        ['Germany','10%'],
+        ['United States','11%'],
+        ['Canada','12%'],
+        ['RU','18%'],
+        ['Ukraine','40%'],
+        ['China','9%']
+    ]);
+
+    var options_map = {
+        backgroundColor: '#f39c12',
+        defaultColor: '#56d4f1',
+        datalessRegionColor: '#efbb88'
+    };
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data, options_map);
 }
