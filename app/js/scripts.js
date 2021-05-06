ffc9e30b797ca8e47aa196a6520a809d597208bf
Koliasick <abrikoskokos42@gmail.com>
2021-05-06 14:42:46 +0300

    all_but_ajax_ready

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -127,7 +127,8 @@ var options_two = {
     markers: {
         size: 4,
         shape: 'circle',
-        colors: ['#ff00d8']
+        colors: ['#ff00d8'],
+        fillColors: ['#ff00d8']
     }
 };
 //
@@ -192,11 +193,18 @@ window.addEventListener('load', function () {
         config_one
     );
     document.getElementById('reload').addEventListener('click',reload);
+    document.getElementById('next').addEventListener('click',_next);
+    document.getElementById('prev').addEventListener('click',_prev);
+    document.getElementById('send').addEventListener('click',_alert);
+    document.getElementById('bot_left').addEventListener('click',display_comments);
+    document.getElementById('bot_middle').addEventListener('click',display_articles);
+    document.getElementById('bot_right').addEventListener('click',display_users);
     var chart = new ApexCharts(document.querySelector("#area_graph"), options_two);
     var bar_chart = new ApexCharts(document.querySelector("#bar_chart"), options);
     chart.render();
     bar_chart.render();
     bigChart.options.animation = false;
+    make_calendar();
 })
 function update_counters()
 {
@@ -205,6 +213,24 @@ function update_counters()
         .then(res=>{
             total_count.innerHTML=res.value;
         });
+}
+function _prev(){
+    if(month_of_today==1){month_of_today=13;}
+    today.setDate(1);
+    today.setMonth(month_of_today-1);
+    day_of_today = today.getDate();
+    weekday_of_today = today.getDay();
+    month_of_today = today.getMonth();
+    make_calendar();
+}
+function _next(){
+    if(month_of_today==12){month_of_today=-1;}
+    today.setDate(1);
+    today.setMonth(month_of_today+1);
+    day_of_today = today.getDate();
+    weekday_of_today = today.getDay();
+    month_of_today = today.getMonth();
+    make_calendar();
 }
 function reload(){
     location.reload();
@@ -228,12 +254,76 @@ function drawRegionsMap() {
     ]);
 
     var options_map = {
+        sizeAxis: { minValue: 0, maxValue: 100 },
         backgroundColor: '#f39c12',
         defaultColor: '#56d4f1',
-        datalessRegionColor: '#efbb88'
+        datalessRegionColor: '#efbb88',
+        width: '100%'
     };
 
     var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
 
     chart.draw(data, options_map);
+}
+
+function _alert(){
+    alert(document.getElementById('massage').value);
+}
+
+const today = new Date();
+let day_of_today = today.getDate();
+let weekday_of_today = today.getDay();
+let month_of_today = today.getMonth();
+const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+const days = ['MO','TU','WE','TH','FR ','SA','SU' ]
+const days_in_month = [31,29,31,30,31,30,31,31,30,31,30,31];
+const my_calendar = $('#calendar');
+const month = $('#month_sel');
+if(weekday_of_today==0)
+{
+ weekday_of_today=7;
+}
+function make_calendar()
+{
+    month.empty();
+    month.append(months[month_of_today]);
+    my_calendar.empty();
+    let f_day = day_of_today%7;
+    f_day=weekday_of_today - f_day;
+    if(f_day<0){f_day = 7 + f_day};
+    for (let i=0;i<7;i++)
+    {
+       my_calendar.append('<div class="p-1 cal_d">'+days[i]+'</div>');
+    }
+    for (let i=0;i<days_in_month[month_of_today]+f_day;i++)
+    {
+        if(i<7&&i<f_day)
+        {
+            my_calendar.append('<div class="p-1"></div>');
+            continue;
+        }
+        if(i-f_day+1==day_of_today)
+        {
+            my_calendar.append('<div id="today" class="cal_el p-1">'+(i-f_day+1)+'</div>');
+            continue;
+        }
+        my_calendar.append('<div class="cal_el p-1">'+(i-f_day+1)+'</div>');
+    }
+}
+
+const bottom_tile = $('#content');
+function display_comments(){
+    bottom_tile.empty();
+    bottom_tile.append('<div class="comment p-3"><img class="avatar" src="images/av1.bmp"><div class="com_text"><div class="ps-1"><sub>by</sub><h6>Jolia</h6></div><div class="ps-1">Its long established fact that a reader will be distracted by a readable content of page when looking at its layout.</div></div></div>' +
+        '    <div class="comment p-3"><img class="avatar" src="images/av2.bmp"><div class="com_text"><div class="ps-1"><sub>by</sub><h6>Deo</h6></div><div class="ps-1">Its long established fact that a reader will be distracted by a readable content of page when looking at its layout.</div></div></div>' +
+        '      <div class="comment p-3"><img class="avatar" src="images/av3.bmp"><div class="com_text"><div class="ps-1"><sub>by</sub><h6>Phionne</h6></div><div class="ps-1">Its long established fact that a reader will be distracted by a readable content of page when looking at its layout.</div></div></div>');
+}
+function display_articles(){
+    bottom_tile.empty();
+    bottom_tile.append('<h6 align="center" class="p-5">No recent articles</h6>');
+}
+function display_users()
+{
+    bottom_tile.empty();
+    bottom_tile.append('<h6 align="center" class="p-5">No new users</h6>');
 }
